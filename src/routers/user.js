2c0825adbb5c51cba9a2fc903eb2e0ddfa0f46b4
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-18 13:59:00 -0300

    cria login de usuario

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 const { Pool } = require('pg')
+const jwt = require('jsonwebtoken')
 
 const pool = new Pool({
     user: 'postgres',
@@ -11,18 +12,12 @@ const pool = new Pool({
 
 const router = require('express').Router()
 
-async function hashSenha(senha) {
-    let salt = crypto.randomBytes(10).toString('hex')
+async function hashSenha(senha, salt) {
+    if (!salt) salt = crypto.randomBytes(10).toString('hex')
     let pbk = await crypto.pbkdf2Sync(senha, salt, 100000, 64, 'sha512')
     let hash = pbk.toString('hex')
     return {hash, salt}
 }
-
-router.get('/postgres', async (req, res) => {
-    const resp = await pool.query('SELECT $1::text as message', ['Hello world!'])
-    res.send(resp.rows[0])
-    await pool.end()
-})
 
 //cadastro usuario
 router.post('/', async (req, res) => {
@@ -37,6 +32,32 @@ router.post('/', async (req, res) => {
         res.send(resp.rows[0])
     }catch(err){
         res.status(500).send(err)
+    }
+})
+
+router.post('/login', async (req,res) => {
+    let sql = 'SELECT *FROM usuario WHERE email = $1'
+    let params = [req.body.email]
+    let users = await pool.query(sql, params)
+
+    if (!users.rows.length){
+        res.status(400).json({err: 'email not found'})
+        return
+    }
+
+    let user = users.rows[0]
+    let {senha, salt} = user
+    let reqSenha = await hashSenha(req.body.senha, salt)
+    console.log(reqSenha)
+
+    if (reqSenha.hash != senha){
+        res.status(400).json({err: 'wrong pass'})
+        return
+    }else{
+        //gera jwt
+        console.log(process.env.JWT)
+        let token = jwt.sign({email: user.email, nome: user.nome}, process.env.JWT, { expiresIn: '8h' })
+        res.status(200).json({token})
     }
 })
 
